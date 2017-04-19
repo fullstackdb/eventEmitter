210460fba31f6d30a5d2e4cd38adceada5153edb
fullstackdb <a@web-ui.org>
2017-04-19 17:46:55 +0300

    update tests and gitignore file

--- test/test.js
+++ test/test.js
@@ -40,4 +40,40 @@ describe('#tiny Event Emitter', function() {
         })
         localEvent.emit('tryAnotherLocalEvent', someData)
     });
+    it('should have emit method', function() {
+        expect(event.default().emit).to.exist;
+    });
+
+    it('should have when method', function() {
+        expect(event.default().emit).to.exist;
+    });
+
+    it('should react to event', function(done) {
+        var localEvent = event.default();
+        localEvent.when('tryLocalEvent', function(data){
+            done();
+        })
+        localEvent.emit('tryLocalEvent', {hasDone: true})
+    });
+
+    it('should send data via event', function(done) {
+        var localEvent = event.default();
+        localEvent.when('trySomeEvent', function(data) {
+            if (data.hasDone) {
+                done();
+            }
+        })
+        localEvent.emit('trySomeEvent', {hasDone: true})
+    });
+
+    it('fetched data should be equel to sended', function(done) {
+        var localEvent = event.default();
+        var someData = {hasDone: true};
+        localEvent.when('tryAnotherLocalEvent', function(data) {
+            if (data === someData) {
+                done();
+            }
+        })
+        localEvent.emit('tryAnotherLocalEvent', someData)
+    });
 });
